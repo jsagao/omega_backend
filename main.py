981e5b750a92a78d82277052fd579ad13812d5f6
James A <jamesa@Jamess-MacBook-Pro.local>
2025-08-23 13:25:14 +0800

    chainged omega_backend main.py and added railway to CORS

--- main.py
+++ main.py
@@ -107,7 +107,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://<your-vercel-domain>.vercel.app",  # add your vercel domain later
+        "https://<your-vercel-domain>.vercel.app", 
+        "http://web-production-5b94.up.railway.app" # add your vercel domain later
     ],
     allow_credentials=True,
     allow_methods=["*"],
